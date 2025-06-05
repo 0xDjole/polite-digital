21c8d27322604c4579384846944bff4e2433bd3b
Djole <0xdjole@gmail.com>
2025-06-05 20:55:47 +0200

    dep

--- deploy/terraform.tf
+++ deploy/terraform.tf
@@ -2,10 +2,4 @@ terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4"
-    }
-  }
 }
